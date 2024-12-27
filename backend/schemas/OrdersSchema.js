3dba18345a559753513d122bf04d3d3c5822d79b
kshitij1439 <kshitijgaikwad142004@gmail.com>
2024-12-27 14:08:10 +0530

    first commit

---
+++ backend/schemas/OrdersSchema.js
@@ -0,0 +1,8 @@
+const {Schema}= require("mongoose");
+const OrdersSchema = new Schema({
+    name: String,
+    qty: Number,
+    percent: String,
+    mode:String,
+});
+module.exports ={OrdersSchema};
